e1c6b5017bf434c1fb5b73f12d569daf3de517ee
Dimitris Zorbas <zorbash@skroutz.gr>
2016-10-30 12:03:45 +0200

    Add truncate JavaScript helper

--- assets/javascripts/helpers.js
+++ assets/javascripts/helpers.js
@@ -29,3 +29,12 @@ export function prepend(value, string) {
 export function append(value, string) {
   return (value != null ? value : '') + (string != null ? string : '');
 }
+
+export function truncate(text, limit, omission) {
+  if (!(typeof limit === 'number')) { limit = 30; }
+  if (!(typeof omission === 'string')) { omission = '…'; }
+
+  if (text.length <= limit) { return text; }
+
+  return `${text.substr(0, limit)}${omission}`;
+}
