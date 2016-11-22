7d1e416311bae253fc03db4bf2a47fdf793f1a38
Dimitris Zorbas <zorbash@skroutz.gr>
2016-11-23 00:24:59 +0200

    Add a streaming Twitter job example

--- jobs/twitter.ex
+++ jobs/twitter.ex
@@ -6,3 +6,11 @@ job :twitter, every: {20, :seconds} do
 
   broadcast!(:twitter, %{items: items})
 end
+
+job :twitter_linux do
+  spawn fn ->
+    for tweet <- ExTwitter.stream_filter(track: "linux") do
+      broadcast!(:twitter_linux, %{text: tweet.text})
+    end
+  end
+end
