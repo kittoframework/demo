21982153f6f38ff818909e04c4d21a1ea6e00b83
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2017-01-12 18:29:12 +0200

    Bump kitto version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Demo.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:kitto, "0.3.2"},
+    [{:kitto, "0.4.0"},
      {:httpoison, "~> 0.9", override: true},
      {:poison, "3.0.0", override: true},
      {:oauth, github: "tim/erlang-oauth"},
